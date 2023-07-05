1d8144dc3e674d8a2ea687d8fc23281caff3ff9f
Marc Contreras Massegu <marc.chuki3@gmail.com>
2023-07-05 15:55:07 +0200

    all champions in a row, still needs improvements tho

--- src/views/champions/Champions.js
+++ src/views/champions/Champions.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react'
+import { CListGroup, CListGroupItem } from '@coreui/react'
 
 const ListComponent = () => {
   const [listItems, setListItems] = useState([])
@@ -15,20 +16,28 @@ const ListComponent = () => {
       .catch((error) => console.error(error))
   }, [])
 
-  return (
-    <div>
-      <ul>
-        {listItems.map((item, index) => {
-          const imagePath = `/assets/images/champions/${item.image}`
+  let content = []
+  for (let i = 0; i < listItems.length; i + 8) {
+    let listEightChampions = listItems.splice(i, i + 8)
+    content.push(extracted(listEightChampions))
+  }
+  return content
+}
 
+function extracted(listEightChampions) {
+  return (
+    <>
+      <CListGroup layout="horizontal">
+        {listEightChampions.map((item, index) => {
+          const imagePath = `/assets/images/champions/${item.image}`
           return (
-            <li key={index}>
+            <CListGroupItem key={item.name}>
               {item.name} - <img src={process.env.PUBLIC_URL + imagePath} alt={item.image} />
-            </li>
+            </CListGroupItem>
           )
         })}
-      </ul>
-    </div>
+      </CListGroup>
+    </>
   )
 }
 
